d41953d2e013a5f526403ba47cf6e556fa7f8dba
Eli Dupree <vcs@elidupree.com>
2017-10-02 18:48:26 -0400

    More work on Monte Carlo AI

--- wesnoth_ai/src/monte_carlo_ai.rs
+++ wesnoth_ai/src/monte_carlo_ai.rs
@@ -8,10 +8,11 @@ use rust_lua_shared::*;
 #[derive (Clone, Serialize, Deserialize, Debug)]
 pub struct Player <LookaheadPlayer: Fn(&State, usize)->Box<fake_wesnoth::Player>> {
   make_player: LookaheadPlayer,
+  root: Node
 }
 
 struct Node {
-  state: State,
+  state: Arc<State>,
   visits: i32,
   moves: Vec<ProposedMove>,
 }
@@ -27,23 +28,23 @@ use fake_wesnoth::{State, Unit, Move};
 
 impl<LookaheadPlayer: Fn(&State, usize)->Box<fake_wesnoth::Player>> fake_wesnoth::Player for Player<LookaheadPlayer> {
   fn choose_move (&mut self, state: & State)->Move {
-    state.locations.iter()
-      .filter_map (| location | location.unit.as_ref())
-      .filter(|unit| unit.side == state.current_side)
-      .flat_map (|unit| possible_unit_moves (state, unit).into_iter())
-      .chain(::std::iter::once (fake_wesnoth::Move::EndTurn))
-      .map (| action | {
-        let evaluation = self.evaluate_move (state, &action);
-        (action, evaluation)
-      })
-      .max_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
-      .unwrap().0
+    self.root = Note {
+      state: Arc::new (state.clone()),
+      visits: 0,
+      moves: Vec::new(),
+    };
+    for _ in 0..100 {
+      self.step ();
+    }
+    self.root.moves.iter()
+      .max_by_key (|a| a.visits)
+      .unwrap().action
   }
 }
 
 impl<LookaheadPlayer: Fn(&State, usize)->Box<fake_wesnoth::Player>> Player<LookaheadPlayer> {
   pub fn new(make_player: LookaheadPlayer)->Self where Self: Sized {Player{make_player: make_player}}
-  pub fn evaluate_move (&self, state: & State, input: & fake_wesnoth::Move)->f64 {
+  /*pub fn evaluate_move (&self, state: & State, input: & fake_wesnoth::Move)->f64 {
     let mut total_score = 0f64;
     let starting_turn = state.turn;
     for _ in 0..100 {
@@ -60,13 +61,20 @@ impl<LookaheadPlayer: Fn(&State, usize)->Box<fake_wesnoth::Player>> Player<Looka
     }
     println!("evaluated move {:?} with score {}", input, total_score);
     total_score
-  }
+  }*/
   
   
   pub fn step (&mut self) {
     let mut node = self.root;
-    while node.moves.is_empty() {
-      node.moves = node.state.locations.iter()
+    while !node.moves.is_empty() {
+      let log_visits = 2.0*(node.visits as f64).ln();
+      let priority = | proposed | proposed.total_score/proposed.visits as f64 + (log_visits/(proposed.visits as f64)).sqrt();
+      let choice = node.moves.iter()
+          .max_by (|a,b| priority(a).cmp(priority(b)))
+          .unwrap().action
+      
+    }
+    node.moves = node.state.locations.iter()
         .filter_map (| location | location.unit.as_ref())
         .filter(|unit| unit.side == state.current_side)
         .flat_map (|unit| possible_unit_moves (state, unit).into_iter())
@@ -84,7 +92,7 @@ impl<LookaheadPlayer: Fn(&State, usize)->Box<fake_wesnoth::Player>> Player<Looka
           Node {
                         state: state_after, visits: 1, moves: Vec::new(),
                       }
-    }
+    
   }
 }
 
